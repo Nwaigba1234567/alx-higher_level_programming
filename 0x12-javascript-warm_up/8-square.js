a9404a503c6e7209eeabad022c8b2956e3935735
Nwaigba1234567 <iamkatty3@gmail.com>
2023-08-27 08:43:38 -0700

    8-square.js file added

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+if (process.argv[2] && !isNaN(process.argv[2])) {
+  const size = parseInt(process.argv[2]);
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+} else {
+  console.log('Missing size');
+}
