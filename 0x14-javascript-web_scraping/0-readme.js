5ec875b425a49e0b39bbad03bc8d53d0a4124b2e
Kate Paul <iamkatty3@gmail.com>
2023-09-27 21:28:19 -0700

    Updated codebase

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+// Check if the file path is provided
+if (process.argv.length < 3) {
+  console.error('Usage: node read-file.js <file-path>');
+  process.exit(1);
+}
+
+// Get the file path
+const filePath = process.argv[2];
+
+// Read the file in UTF-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
